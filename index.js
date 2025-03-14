e9e98affd65c6ea5693d1f30ea7382bdfd25e946
Aaron McAlister <aaronmcalister@icloud.com>
2025-03-14 21:02:49 +0000

    Finalised Version

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
+// create array of options
+const options = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"];
-    return options[Math.floor(Math.random() * options.length)];
+    // generate random choice
+    const randomString = Math.floor(Math.random() * options.length);
+    return options[randomString];
 }
 
 function getHumanChoice() {
-    // const computerChoice = getComputerChoice(); // Generate computer choice first
-    // console.log("Computer chose:", computerChoice); // Log computer's choice for testing
-
-    const choice = prompt(`Pick Rock, Paper or Scissors:`).toLowerCase(); // Convert input to lowercase
-
-    if (choice === computerChoice) {
-        alert("It was a tie!");
-    } else if (
-        (choice === "paper" && computerChoice === "rock") ||
-        (choice === "rock" && computerChoice === "scissors") ||
-        (choice === "scissors" && computerChoice === "paper")
-    ) {
-        alert("You win!");
-    } else {
-        alert("You lose!");
-    }
+    // Converts all input to lowercase
+    const choice = prompt(`Pick Rock, Paper or Scissors:`).toLowerCase();
+    return choice;
 }
-
-getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
-    
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert(`You tied! You both picked ${computerChoice}!`); // tie
+    } else if (
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        alert(`You win! ${humanChoice} beats ${computerChoice}`); // win
+        humanScore++;
+    } else {
+        alert(`You lose! ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    }
 }
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        alert(`Round ${i+1}:`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    alert(`Final Scores are: You ${humanScore} : Computer ${computerScore}.`);
+}
+
+playGame();
